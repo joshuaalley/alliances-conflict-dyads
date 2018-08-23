52f8785e7e1607ae9056991f5c2915105d58d4ec
Josh Alley <jkalley14@tamu.edu>
2018-08-23 11:22:25 -0500

    cluster robust variance estimation- kenwick and vasquez 2017

---
+++ Kenwick and Vasquez 2017/Kenwick and Vasquez reanalysis.R
@@ -0,0 +1,105 @@
+# Joshua Alley
+# Texas A&M University
+# Replication and check of Johnson and Leeds 2017 
+# model of alliance participation and conflict onset
+
+
+# Load packages
+library(here)
+library(haven)
+library(arm)
+library(MASS)
+library(dplyr)
+library(ggplot2)
+library(separationplot)
+library(sandwich)
+library(lmtest)
+library(xtable)
+
+
+# Set working directory to current folder 
+setwd(here::here())
+getwd()
+
+
+
+
+# Load data
+kv.2017 <- read.csv("Kenwick and Vasquez 2017/kenwick-vasquez-2017.csv")
+
+
+### Robustness Check: cluster robust variance estimator 
+
+# Start by creating a dataframe with complete cases
+kv.2017.comp <- select(kv.2017, dispute, fbdef, nfbdef, pchaloff, pchalneu,
+                       ln_distance, capprop, jdem, s_un_glo, 
+                       peaceyrs, peaceyrs2, peaceyrs3, challenger, target, ddyad)
+kv.2017.comp <- kv.2017.comp[complete.cases(kv.2017.comp), ]
+
+
+# Run the model on complete data
+kv.2017.model <- glm(dispute ~ fbdef + nfbdef + pchaloff +
+                       pchalneu + ln_distance + capprop + jdem + s_un_glo +
+                       peaceyrs + peaceyrs2 + peaceyrs3,
+                     family = binomial(link = "logit"),
+                     data = kv.2017.comp, x = T)
+summary(kv.2017.model) # This replicates their results w/o corrected standard errors
+
+
+# Implement Aronow et al clustered standard errors 
+# accounts for participation of states in multiple dyads 
+# Cluster robust variance estimation function
+robust.se.nodfc <- function(model, cluster){
+  require(sandwich)
+  require(lmtest)
+  M <- length(unique(cluster))
+  N <- length(cluster)
+  K <- model$rank
+  dfc <- 1
+  uj <- apply(estfun(model), 2, function(x) tapply(x, cluster, sum))
+  rcse.cov <- dfc * sandwich(model, meat = crossprod(uj)/N)
+  rcse.se <- coeftest(model, rcse.cov)
+  return(list(rcse.cov, rcse.se))
+}
+
+
+# Create an index for the dyad members
+kv.index.sc <- unique(c(kv.2017.comp$challenger, kv.2017.comp$target))
+kv.index <- index.sc[order(index.sc)]
+kv.dyad.mat <- kv.2017.comp[,c("challenger","target")]
+
+
+# Correct the standard errors
+# Dyadic cluster robust via multiway decomposition
+for(i in 1:length(kv.index)){
+  iUp <- kv.index[i]
+  kv.clusUp <- apply(kv.dyad.mat, 
+                     1,
+                     function(x)as.numeric(iUp %in% x))
+  kv.clusIndexUp <- kv.clusUp*-99 + (1-kv.clusUp)*1:nrow(kv.dyad.mat)
+  if(i==1){kv.dcrUp <- robust.se.nodfc(kv.2017.model, kv.clusIndexUp)[[1]]}
+  if(i>1){kv.dcrUp <- kv.dcrUp + robust.se.nodfc(kv.2017.model, kv.clusIndexUp)[[1]]}	
+  cat(paste(iUp, " ")); flush.console()
+}
+
+# substract naive CR:
+kv.dcrUp2 <- kv.dcrUp - robust.se.nodfc(kv.2017.model, kv.2017.comp$ddyad)[[1]]
+# substract HR:
+kv.Vhat <- kv.dcrUp2 - (length(index)-2)*vcovHC(kv.2017.model,type="HC0")
+kv.Vhat <- as.matrix(kv.Vhat)
+
+# Standard errors and test statistics
+kv.dcr_se <- sqrt(diag(kv.Vhat))
+kv.stat.sig <- abs(coef(kv.2017.model)/sqrt(diag(kv.Vhat))) > 1.64
+
+kv.V1 <- vcov(kv.2017.model)
+kv.se_nc <- sqrt(diag(kv.V1))
+
+
+# Export the results
+results.kv.corr <- cbind(kv.2017.model$coefficients, kv.dcr_se, kv.stat.sig)
+print(results.kv.corr)
+
+
+xtable(kv.2017.model)
+xtable(results.kv.corr)
